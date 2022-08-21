8785d1a471b7d9bf39aeebf43c49f9193fb3eb53
KB0206 <kumkumbarot10@gmail.com>
2022-08-21 20:45:32 +0530

    Practical1_1

---
+++ src/main/kotlin/practical1_1.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val a_int:Int = 2
+    val b_double: Double = 2.6557765
+    val c_float: Float =  6.98f
+    val d_long: Long = 20012011009
+    val e_short: Short = -78
+    val f_byte: Byte = 12
+    val g_char: Char = 'K'
+    val h_boolean: Boolean = true
+    val i_string:String = "Kumkum"
+    println(" Enrollment no:20012011009 \n Name:Kumkum Barot \n Integer value = $a_int \n Float value = $c_float \n Character value = $g_char \n String value = $i_string \n Boolean value = $h_boolean \n Double value = $b_double \n Long value = $d_long \n Short value = $e_short \n Byte value = $f_byte ")}
+//1.Store & Display values in different variable of different type (Integer, Double, Float, Long, Short, Byte, Char, Boolean, String)
